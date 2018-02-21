90ac7cee5857fbdf476d4a75b9e315dcf4c6b9b5
rfigueiredo0622 <rfigueiredo0622@gmail.com>
2018-02-20 21:24:15 -0300

    primeiro commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router/src/config';
+import { HomeComponent } from 'app/home/home.component';
+import { AboutComponent } from 'app/about/about.component';
+import { RestaurantsComponent } from 'app/restaurants/restaurants.component';
+
+export const ROUTES: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'restaurants', component: RestaurantsComponent }
+];
