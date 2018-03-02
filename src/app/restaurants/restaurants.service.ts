5c3310897cef7673c5e5ddf78e6137971ed1c142
rfigueiredo0622 <rfigueiredo0622@gmail.com>
2018-03-02 10:27:00 -0300

    Criando a classe de serviço - corrigindo alertas de compilação

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,25 +1,25 @@
-import { Restaurant } from "app/restaurants/restaurant/restaurant.model";
+import { Restaurant } from 'app/restaurants/restaurant/restaurant.model';
 
 export class RestaurantsService {
 
     rests: Restaurant[] = [
         {
-            id: "bread-bakery",
-            name: "Bread & Bakery",
-            category: "Bakery",
+            id: 'bread-bakery',
+            name: 'Bread & Bakery',
+            category: 'Bakery',
             deliveryEstimate: '25m',
             rating: 4.9,
-            imagePath: "assets/img/restaurants/breadbakery.png"     
+            imagePath: 'assets/img/restaurants/breadbakery.png'
           },
           {
-            id: "burger-house",
-            name: "Burger House",
-            category: "Hamburgers",
-            deliveryEstimate: "100m",
+            id: 'burger-house',
+            name: 'Burger House',
+            category: 'Hamburgers',
+            deliveryEstimate: '100m',
             rating: 3.5,
-            imagePath: "assets/img/restaurants/burgerhouse.png"
+            imagePath: 'assets/img/restaurants/burgerhouse.png'
           }
-    ]
+    ];
 
     constructor() { }
 
