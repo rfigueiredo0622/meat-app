de183e0c12edfdc08fc113f488c0bacd4f5bd2b1
rfigueiredo0622 <rfigueiredo0622@gmail.com>
2018-03-20 10:02:23 -0300

    48 - Rotas Filhas para Avaliações e Menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,17 @@ import { HomeComponent } from 'app/home/home.component';
 import { AboutComponent } from 'app/about/about.component';
 import { RestaurantsComponent } from 'app/restaurants/restaurants.component';
 import { RestaurantDetailComponent } from 'app/restaurant-detail/restaurant-detail.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { ReviewsComponent } from './reviews/reviews.component';
 
 export const ROUTES: Routes = [
     { path: '', component: HomeComponent },
     { path: 'restaurants', component: RestaurantsComponent },
-    { path: 'restaurants/:id', component: RestaurantDetailComponent },
+    { path: 'restaurants/:id', component: RestaurantDetailComponent,
+        children: [
+            { path: '', redirectTo: 'menu', pathMatch: 'full' }, // torna o MenuComponent default.
+            { path: 'menu', component: MenuComponent },
+            { path: 'reviews', component: ReviewsComponent }
+        ]},
     { path: 'about', component: AboutComponent }
 ];
