1a9a270bb9432a3fb2f28171a1af637fa9c3c0ad
rfigueiredo0622 <rfigueiredo0622@gmail.com>
2018-05-14 17:26:30 -0300

    68 - Finalizando a Compra (HTTP POST)

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
 import { ShoppingCartService } from '../restaurant-detail/shopping-cart/shopping-cart.service';
 import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
+import { Order, OrderItem } from './order.model';
+import { Observable } from 'rxjs/Observable';
+import {Http, Headers, RequestOptions} from '@angular/http';
+import {MEAT_API} from '../app.api';
+
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class OrderService {
-    constructor(private cartService: ShoppingCartService) {}
+  
+    constructor(private cartService: ShoppingCartService, private http: Http) {}
 
     carItems(): CartItem[] {
         return this.cartService.items;
@@ -26,4 +33,17 @@ export class OrderService {
         return this.cartService.total();
     }
 
+    checkOrder(order: Order): Observable<string> {
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+
+        return this.http.post(`${MEAT_API}/orders`,
+                            JSON.stringify(order),
+                            new RequestOptions({headers: headers}))
+                        .map(response => response.json());
+    }
+
+    clear() {
+        this.cartService.clear();
+    }
 }
