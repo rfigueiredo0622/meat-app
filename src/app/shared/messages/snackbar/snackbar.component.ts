045ceb773e6bec8c7d06d4974dea9b94769524c6
rfigueiredo0622 <rfigueiredo0622@gmail.com>
2018-06-19 14:29:41 -0300

    85 - Aplicando Animações no Snackbar

--- src/app/shared/messages/snackbar/snackbar.component.ts
+++ src/app/shared/messages/snackbar/snackbar.component.ts
@@ -1,17 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import {trigger, state, style, transition, animate} from '@angular/animations';
 
 @Component({
   selector: 'mt-snackbar',
   templateUrl: './snackbar.component.html',
-  styleUrls: ['./snackbar.component.css']
+  styleUrls: ['./snackbar.component.css'],
+  animations: [
+    trigger('snack-visibility', [
+      state('hidden', style({
+        opacity: 0,
+        bottom: '0px'
+      })),
+      state('visible', style({
+        opacity: 1,
+        bottom: '30px'
+
+      })),
+      transition('hidden => visible', animate('500ms 0s easy-in')),
+      transition('visible => hidden', animate('500ms 0s easy-out')),
+    ])
+  ]
 })
 export class SnackbarComponent implements OnInit {
 
   message: string = 'Hello there!';
+  snackVisibility: string = 'hidden';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleSnack() {
+    this.snackVisibility = this.snackVisibility === 'hidden' ? 'visible' : 'hidden';
+  }
 }
