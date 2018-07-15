b00453323a897907e3f77309c7f418f70eb36c88
rfigueiredo0622 <rfigueiredo0622@gmail.com>
2018-07-15 18:04:22 -0300

    86 - Usando Observable com Snackbar

--- src/app/shared/messages/snackbar/snackbar.component.ts
+++ src/app/shared/messages/snackbar/snackbar.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {trigger, state, style, transition, animate} from '@angular/animations';
+import { NotificationService } from '../notification.service';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/observable/timer';
 
 @Component({
   selector: 'mt-snackbar',
@@ -16,19 +20,24 @@ import {trigger, state, style, transition, animate} from '@angular/animations';
         bottom: '30px'
 
       })),
-      transition('hidden => visible', animate('500ms 0s easy-in')),
-      transition('visible => hidden', animate('500ms 0s easy-out')),
+      transition('hidden => visible', animate('500ms 0s ease-in')),
+      transition('visible => hidden', animate('500ms 0s ease-out')),
     ])
   ]
 })
 export class SnackbarComponent implements OnInit {
 
-  message: string = 'Hello there!';
+  message: string;
   snackVisibility: string = 'hidden';
 
-  constructor() { }
+  constructor(private notificationService: NotificationService) { }
 
   ngOnInit() {
+    this.notificationService.notifier.subscribe(message => {
+      this.message = message;
+      this.snackVisibility = 'visible';
+      Observable.timer(3000).subscribe(timer => this.snackVisibility = 'hidden');
+    });
   }
 
   toggleSnack() {
